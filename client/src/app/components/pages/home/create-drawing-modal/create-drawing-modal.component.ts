557cd6283b7f4f786ddd617f139867492df23797
Simon Tran <simon.tran@polymtl.ca>
2020-01-22 18:29:52 -0500

    LOG2990-26 - Added create button to CreateDrawingModal

--- client/src/app/components/pages/home/create-drawing-modal/create-drawing-modal.component.ts
+++ client/src/app/components/pages/home/create-drawing-modal/create-drawing-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { Router } from '@angular/router';
 import { AbstractModalComponent } from '../../../shared/abstract-modal/abstract-modal.component';
 
 @Component({
@@ -8,7 +9,16 @@ import { AbstractModalComponent } from '../../../shared/abstract-modal/abstract-
     styleUrls: ['./create-drawing-modal.component.scss'],
 })
 export class CreateDrawingModalComponent extends AbstractModalComponent {
-    constructor(dialogRef: MatDialogRef<AbstractModalComponent>) {
+    width = 0;
+    height = 0;
+    color = 0;
+
+    constructor(private router: Router, public dialogRef: MatDialogRef<AbstractModalComponent>) {
         super(dialogRef);
+    }
+
+    onCreateClick() {
+        const params = { width: this.width, height: this.height, color: this.color };
+        this.router.navigate(['edit', { ...params }]).then(() => this.dialogRef.close());
     }
 }
