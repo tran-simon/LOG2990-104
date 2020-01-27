7196e38285b213aac661f0f46db8520a6eb75d0a
Simon Malouin <simon.malouin@polymtl.ca>
2020-01-27 18:24:46 -0500

    LOG2990-17-minor-cleanup

--- client/src/app/components/pages/editor/editor/editor.component.ts
+++ client/src/app/components/pages/editor/editor/editor.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-// import { DrawingSurface } from '../drawing-surface';
 
 export interface EditorParams {
     surfaceWidth: number;
