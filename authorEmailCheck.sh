ebd71cb1e97600176e519bfa21ef46497573a246
Simon Tran <simon.tran@polymtl.ca>
2020-01-28 17:31:33 +0000

    LOG2990-11 - authorEmailCheck.sh edited online with Bitbucket

--- authorEmailCheck.sh
+++ authorEmailCheck.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Script to prevent developpers from commiting with their wrong emails. Pipeline will fail if a user commits with an email other than "...@polymtl.ca"
 
 RESULT=$(git shortlog -sne --all | grep -v "polymtl")
 
