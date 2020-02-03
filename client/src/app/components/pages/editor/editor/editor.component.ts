06bb4c028421d438aa5c3711cfa8ba5142869e7c
Simon Tran <simon.tran@polymtl.ca>
2020-02-03 14:03:01 -0500

    LOG2990-49 - Removed console.trace

--- client/src/app/components/pages/editor/editor/editor.component.ts
+++ client/src/app/components/pages/editor/editor/editor.component.ts
@@ -1,73 +1,64 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Color} from 'src/app/utils/color/color';
-import {KeyboardEventHandler} from 'src/app/utils/events/KeyboardEventHandler';
-import {KeyboardListener} from 'src/app/utils/events/KeyboardListener';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Color } from 'src/app/utils/color/color';
+import { KeyboardEventHandler } from 'src/app/utils/events/KeyboardEventHandler';
+import { KeyboardListener } from 'src/app/utils/events/KeyboardListener';
 
 export interface EditorParams {
-  surfaceWidth: number;
-  surfaceHeight: number;
-  surfaceColor: Color;
+    surfaceWidth: number;
+    surfaceHeight: number;
+    surfaceColor: Color;
 }
 
 @Component({
-  selector: 'app-editor',
-  templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss'],
+    selector: 'app-editor',
+    templateUrl: './editor.component.html',
+    styleUrls: ['./editor.component.scss'],
 })
 export class EditorComponent implements OnInit, KeyboardEventHandler {
-  params: EditorParams = {surfaceColor: Color.WHITE, surfaceHeight: 0, surfaceWidth: 0};
-  keyboardListener: KeyboardListener = new KeyboardListener(this);
+    params: EditorParams = { surfaceColor: Color.WHITE, surfaceHeight: 0, surfaceWidth: 0 };
+    keyboardListener: KeyboardListener = new KeyboardListener(this);
 
-  constructor(private router: ActivatedRoute) {
-  }
+    constructor(private router: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.router.params.subscribe((params) => {
-      this.params.surfaceWidth = params.width ? +params.width : 500;
-      this.params.surfaceHeight = params.height ? +params.height : 300;
-      this.params.surfaceColor = params.color ? Color.hex(params.color) : Color.WHITE;
-    });
-  }
+    ngOnInit() {
+        this.router.params.subscribe((params) => {
+            this.params.surfaceWidth = params.width ? +params.width : 500;
+            this.params.surfaceHeight = params.height ? +params.height : 300;
+            this.params.surfaceColor = params.color ? Color.hex(params.color) : Color.WHITE;
+        });
+    }
 
-  ctrlA(event: KeyboardEvent): boolean {
-    console.trace('ctrlA');
-    return true;
-  }
+    ctrlA(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  ctrlC(event: KeyboardEvent): boolean {
-    console.trace('ctrlC');
-    return true;
-  }
+    ctrlC(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  ctrlD(event: KeyboardEvent): boolean {
-    console.trace('ctrlD');
-    return true;
-  }
+    ctrlD(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  ctrlShiftZ(event: KeyboardEvent): boolean {
-    console.trace('ctrlShiftZ');
-    return true;
-  }
+    ctrlShiftZ(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  ctrlV(event: KeyboardEvent): boolean {
-    console.trace('ctrlV');
-    return true;
-  }
+    ctrlV(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  ctrlX(event: KeyboardEvent): boolean {
-    console.trace('ctrlX');
-    return true;
-  }
+    ctrlX(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  ctrlZ(event: KeyboardEvent): boolean {
-    console.trace('ctrlZ');
-    return true;
-  }
+    ctrlZ(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  @HostListener('window:keydown', ['$event'])
-  keyDown(event: KeyboardEvent): void {
-    this.keyboardListener.keyDown(event);
-  }
-
+    @HostListener('window:keydown', ['$event'])
+    keyDown(event: KeyboardEvent): void {
+        this.keyboardListener.keyDown(event);
+    }
 }
--- client/src/app/components/pages/home/home/home.component.ts
+++ client/src/app/components/pages/home/home/home.component.ts
@@ -1,59 +1,57 @@
-import {Component, HostListener} from '@angular/core';
-import {MatDialog} from '@angular/material';
-import {Router} from '@angular/router';
-import {KeyboardListener} from 'src/app/utils/events/KeyboardListener';
-import {KeyboardEventHandler} from 'src/app/utils/events/KeyboardEventHandler';
-import {CreateDrawingModalComponent} from '../create-drawing-modal/create-drawing-modal.component';
+import { Component, HostListener } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
+import { KeyboardEventHandler } from 'src/app/utils/events/KeyboardEventHandler';
+import { KeyboardListener } from 'src/app/utils/events/KeyboardListener';
+import { CreateDrawingModalComponent } from '../create-drawing-modal/create-drawing-modal.component';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements KeyboardEventHandler {
-  previousDrawings = false;
-  keyboardListener: KeyboardListener = new KeyboardListener(this);
-  modalIsOpened = false;
+    previousDrawings = false;
+    keyboardListener: KeyboardListener = new KeyboardListener(this);
+    modalIsOpened = false;
 
-  constructor(
-    private router: Router, private dialog: MatDialog) {
-  }
+    constructor(private router: Router, private dialog: MatDialog) {}
 
-  openCreateModal(): void {
-    if (!this.modalIsOpened) {
-      this.dialog.open(CreateDrawingModalComponent, {}).afterClosed().subscribe(() => {
-        this.modalIsOpened = false;
-      });
-      this.modalIsOpened = true;
+    openCreateModal(): void {
+        if (!this.modalIsOpened) {
+            this.dialog
+                .open(CreateDrawingModalComponent, {})
+                .afterClosed()
+                .subscribe(() => {
+                    this.modalIsOpened = false;
+                });
+            this.modalIsOpened = true;
+        }
     }
-  }
 
-  openPage(nextLink: string): void {
-    this.router.navigate([nextLink]);
-  }
+    openPage(nextLink: string): void {
+        this.router.navigate([nextLink]);
+    }
 
-  @HostListener('window:keydown', ['$event'])
-  keyDown(event: KeyboardEvent): void {
-    this.keyboardListener.keyDown(event);
-  }
+    @HostListener('window:keydown', ['$event'])
+    keyDown(event: KeyboardEvent): void {
+        this.keyboardListener.keyDown(event);
+    }
 
-  ctrlO(event: KeyboardEvent): boolean {
-    this.openCreateModal();
-    return true;
-  }
+    ctrlO(event: KeyboardEvent): boolean {
+        this.openCreateModal();
+        return true;
+    }
 
-  ctrlE(event: KeyboardEvent): boolean {
-    console.trace('exporter');
-    return true;
-  }
+    ctrlE(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  ctrlG(event: KeyboardEvent): boolean {
-    console.trace('galerie');
-    return true;
-  }
+    ctrlG(event: KeyboardEvent): boolean {
+        return true;
+    }
 
-  ctrlS(event: KeyboardEvent): boolean {
-    console.trace('sauvegarder');
-    return true;
-  }
+    ctrlS(event: KeyboardEvent): boolean {
+        return true;
+    }
 }
