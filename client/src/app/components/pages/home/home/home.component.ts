b68b8bf55cd548860ee761b8cd6b944d265d6951
Simon Tran <simon.tran@polymtl.ca>
2020-02-04 17:29:17 -0500

    LOG2990-49 - reformatted

--- client/src/app/components/pages/home/home/home.component.ts
+++ client/src/app/components/pages/home/home/home.component.ts
@@ -6,52 +6,52 @@ import { KeyboardListener } from 'src/app/utils/events/KeyboardListener';
 import { CreateDrawingModalComponent } from '../create-drawing-modal/create-drawing-modal.component';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements KeyboardEventHandler {
-    previousDrawings = false;
-    keyboardListener: KeyboardListener = new KeyboardListener(this);
-    modalIsOpened = false;
+  previousDrawings = false;
+  keyboardListener: KeyboardListener = new KeyboardListener(this);
+  modalIsOpened = false;
 
-    constructor(private router: Router, private dialog: MatDialog) {}
+  constructor(private router: Router, private dialog: MatDialog) {}
 
-    openCreateModal(): void {
-        if (!this.modalIsOpened) {
-            this.dialog
-                .open(CreateDrawingModalComponent, {})
-                .afterClosed()
-                .subscribe(() => {
-                    this.modalIsOpened = false;
-                });
-            this.modalIsOpened = true;
-        }
+  openCreateModal(): void {
+    if (!this.modalIsOpened) {
+      this.dialog
+        .open(CreateDrawingModalComponent, {})
+        .afterClosed()
+        .subscribe(() => {
+          this.modalIsOpened = false;
+        });
+      this.modalIsOpened = true;
     }
+  }
 
-    openPage(nextLink: string): void {
-        this.router.navigate([nextLink]);
-    }
+  openPage(nextLink: string): void {
+    this.router.navigate([nextLink]);
+  }
 
-    @HostListener('window:keydown', ['$event'])
-    keyDown(event: KeyboardEvent): void {
-        this.keyboardListener.keyDown(event);
-    }
+  @HostListener('window:keydown', ['$event'])
+  keyDown(event: KeyboardEvent): void {
+    this.keyboardListener.keyDown(event);
+  }
 
-    ctrlO(event: KeyboardEvent): boolean {
-        this.openCreateModal();
-        return true;
-    }
+  ctrlO(event: KeyboardEvent): boolean {
+    this.openCreateModal();
+    return true;
+  }
 
-    ctrlE(event: KeyboardEvent): boolean {
-        return true;
-    }
+  ctrlE(event: KeyboardEvent): boolean {
+    return true;
+  }
 
-    ctrlG(event: KeyboardEvent): boolean {
-        return true;
-    }
+  ctrlG(event: KeyboardEvent): boolean {
+    return true;
+  }
 
-    ctrlS(event: KeyboardEvent): boolean {
-        return true;
-    }
+  ctrlS(event: KeyboardEvent): boolean {
+    return true;
+  }
 }
