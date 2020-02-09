7ae7a3b94438b846f2d3b4a9d76c29d688cfc846
Simon Tran <simon.tran@polymtl.ca>
2020-02-09 10:54:40 -0500

    LOG2990-52 - Moved error-messages from custom-input to shared/inputs.

--- client/src/app/utils/events/keyboard-listener.ts
+++ client/src/app/utils/events/keyboard-listener.ts
@@ -1,6 +1,10 @@
 import { KeyboardEventHandler } from 'src/app/utils/events/keyboard-event-handler';
 
 export class KeyboardListener {
+  /**
+   * Handles keyDown event with KeyboardEventHandler.
+   * It looks for a function in the handler following KeyboardEventHandler pattern (ie: 'ctrl_shift_o')
+   */
   static keyDown(event: KeyboardEvent, handler: KeyboardEventHandler): boolean {
     let success: boolean;
     let prefix = event.ctrlKey ? 'ctrl_' : '';
