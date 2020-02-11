b9e05dbf17711036beb11ddd51077dd60378cb4a
Simon Tran <simon.tran@polymtl.ca>
2020-02-11 17:05:30 -0500

    LOG2990-69 - Added tests to AbstractCanvasDrawer

--- client/src/app/components/shared/abstract-canvas-drawer/abstract-canvas-drawer.spec.ts
+++ client/src/app/components/shared/abstract-canvas-drawer/abstract-canvas-drawer.spec.ts
@@ -23,10 +23,14 @@ describe('AbstractCanvasDrawer', () => {
   }
 
   const component: AbstractCanvasDrawer = new AbstractCanvasDrawerImpl();
-
   let drawSpy: Spy;
+  let updateColorSpy: Spy;
+  let colorChangedSpy: Spy;
+
   beforeEach(() => {
     drawSpy = spyOn(component, 'draw');
+    updateColorSpy = spyOn(component, 'updateColor').and.callThrough();
+    colorChangedSpy = spyOn(component.colorChanged, 'emit');
   });
 
   it('should draw after view init', () => {
@@ -43,14 +47,42 @@ describe('AbstractCanvasDrawer', () => {
     expect(drawSpy).toHaveBeenCalled();
   });
 
+  it('should redraw on updateColor if shouldRedraw returns true', () => {
+    component.updateColor(Color.RED);
+    expect(drawSpy).toHaveBeenCalled();
+    expect(colorChangedSpy).toHaveBeenCalledWith(Color.RED);
+  });
+
   it('should not redraw if shouldRedraw returns false', () => {
     component.updateColor(Color.BLUE);
     expect(drawSpy).not.toHaveBeenCalled();
+    expect(colorChangedSpy).toHaveBeenCalledWith(Color.BLUE);
   });
 
   it('sets mouseIsDown to true when mouse is down', () => {
     component.onMouseDown({} as MouseEvent);
     expect(component.mouseIsDown).toEqual(true);
+  });
+
+  it('updates color on mouse down', () => {
+    component.onMouseDown({} as MouseEvent);
+
+    expect(updateColorSpy).toHaveBeenCalledWith(Color.RED);
+  });
+
+  it('updates color on mouse move if mouse is down', () => {
+    component.onMouseDown({} as MouseEvent);
+    component.onMouseMove({} as MouseEvent);
+
+    expect(updateColorSpy).toHaveBeenCalledWith(Color.RED);
+  });
+
+  it('does not update color on mouse move if mouse is not down', () => {
+    component.onMouseDown({} as MouseEvent);
+    component.onMouseUp();
+    component.onMouseMove({} as MouseEvent);
+
+    expect(updateColorSpy).toHaveBeenCalledTimes(1);
   });
 
   it('sets mouseIsDown to false when mouse is up', () => {
