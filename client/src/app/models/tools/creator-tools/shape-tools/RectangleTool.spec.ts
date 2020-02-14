88455410b420d4ca1549df00d3bdfbb21509c37d
Simon Malouin <simon.malouin@polymtl.ca>
2020-02-14 00:24:12 -0500

    LOG2990-62 completed tests for RectangleTool.ts

--- client/src/app/models/tools/creator-tools/shape-tools/RectangleTool.spec.ts
+++ client/src/app/models/tools/creator-tools/shape-tools/RectangleTool.spec.ts
@@ -2,6 +2,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DrawingSurfaceComponent } from 'src/app/components/pages/editor/drawing-surface/drawing-surface.component';
 import { Coordinate } from 'src/app/models/Coordinate';
+import { RectangleContourType, RectangleToolProperties } from 'src/app/models/ToolProperties/RectangleToolProperties';
+import { Color } from 'src/app/utils/color/color';
 import { RectangleTool } from './RectangleTool';
 
 describe('ShapeTool', () => {
@@ -9,45 +11,7 @@ describe('ShapeTool', () => {
   let fixture: ComponentFixture<DrawingSurfaceComponent>;
   let surface: DrawingSurfaceComponent;
 
-  /*const mouseDown = (c: Coordinate = new Coordinate()): MouseEvent => {
-    return {
-      type: 'mousedown',
-      offsetX: c.x,
-      offsetY: c.y,
-    } as MouseEvent;
-  };
-
-  const mouseMove = (c: Coordinate = new Coordinate()): MouseEvent => {
-    return {
-      type: 'mousemove',
-      offsetX: c.x,
-      offsetY: c.y,
-    } as MouseEvent;
-  };
-
-  const mouseUp = (c: Coordinate = new Coordinate()): MouseEvent => {
-    return {
-      type: 'mouseup',
-      offsetX: c.x,
-      offsetY: c.y,
-    } as MouseEvent;
-  };
-
-  const keyUp = (key: string, shiftKey: boolean = false): KeyboardEvent => {
-    return {
-      key,
-      type: 'keyup',
-      shiftKey,
-    } as KeyboardEvent;
-  };
-
-  const keyDown = (key: string, shiftKey: boolean = false): KeyboardEvent => {
-    return {
-      key,
-      type: 'keydown',
-      shiftKey,
-    } as KeyboardEvent;
-  };*/
+  const properties = new RectangleToolProperties(Color.RED, Color.GREEN); // todo - include default in constructor
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -63,12 +27,14 @@ describe('ShapeTool', () => {
   });
 
   it('can initialize new Rectangle', () => {
+    rectangleTool.toolProperties = properties;
     rectangleTool.initShape(new Coordinate(100, 100));
     expect(rectangleTool.shape.origin).toEqual(new Coordinate(100, 100));
     expect(surface.svg.nativeElement.querySelector('rect')).toBeTruthy();
   });
 
   it('can resize Rectangle', () => {
+    rectangleTool.toolProperties = properties;
     rectangleTool.initShape(new Coordinate(100, 100));
     rectangleTool.resizeShape(new Coordinate(75, 50));
     expect(rectangleTool.shape.width).toEqual(75);
@@ -76,10 +42,38 @@ describe('ShapeTool', () => {
   });
 
   it('can resize and reposition Rectangle', () => {
+    rectangleTool.toolProperties = properties;
     rectangleTool.initShape(new Coordinate(100, 100));
     rectangleTool.resizeShape(new Coordinate(75, 50), new Coordinate(125, 125));
     expect(rectangleTool.shape.origin).toEqual(new Coordinate(125, 125));
     expect(rectangleTool.shape.width).toEqual(75);
     expect(rectangleTool.shape.height).toEqual(50);
+  });
+
+  it('can draw Rectangle contour and fill', () => {
+    properties.contourType = RectangleContourType.FILLEDCONTOUR;
+    rectangleTool.toolProperties = properties;
+    rectangleTool.initShape(new Coordinate(100, 100));
+    const style = rectangleTool.shape.svgNode.style;
+    expect(style.fill ? style.fill.replace(/ /g, '') : '').toEqual(properties.primaryColor.rgbString);
+    expect(style.stroke ? style.stroke.replace(/ /g, '') : '').toEqual(properties.secondaryColor.rgbString);
+  });
+
+  it('can draw Rectangle fill only', () => {
+    properties.contourType = RectangleContourType.FILLED;
+    rectangleTool.toolProperties = properties;
+    rectangleTool.initShape(new Coordinate(100, 100));
+    const style = rectangleTool.shape.svgNode.style;
+    expect(style.fill ? style.fill.replace(/ /g, '') : '').toEqual(properties.primaryColor.rgbString);
+    expect(style.strokeWidth).toEqual('0');
+  });
+
+  it('can draw Rectangle contour only', () => {
+    properties.contourType = RectangleContourType.CONTOUR;
+    rectangleTool.toolProperties = properties;
+    rectangleTool.initShape(new Coordinate(100, 100));
+    const style = rectangleTool.shape.svgNode.style;
+    expect(style.fillOpacity).toEqual('0');
+    expect(style.stroke ? style.stroke.replace(/ /g, '') : '').toEqual(properties.secondaryColor.rgbString);
   });
 });
