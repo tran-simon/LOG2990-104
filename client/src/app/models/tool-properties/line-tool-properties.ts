cc2fefd20618725c717ad99743c1c303ba78dcd5
Samuel Poulin <samuel.poulin@polymtl.ca>
2020-02-17 14:45:25 -0500

    LOG2990-118 refactored tool properties and tests

--- client/src/app/models/tool-properties/line-tool-properties.ts
+++ client/src/app/models/tool-properties/line-tool-properties.ts
@@ -1,4 +1,5 @@
 import { ToolProperties } from 'src/app/models/tool-properties/tool-properties';
+import { MathUtil } from '../../utils/math/math-util';
 
 export enum LineJunctionType {
   POINTS = 'Avec points',
@@ -11,24 +12,37 @@ export class LineToolProperties extends ToolProperties {
   static readonly MIN_DIAMETER = 1;
   static readonly MAX_DIAMETER = 50;
 
-  minThickness = LineToolProperties.MIN_THICKNESS;
-  maxThickness = LineToolProperties.MAX_THICKNESS;
-  minDiameter = LineToolProperties.MIN_DIAMETER;
-  maxDiameter = LineToolProperties.MAX_DIAMETER;
+  private readonly _minDiameter = LineToolProperties.MIN_DIAMETER;
+  private readonly _maxDiameter = LineToolProperties.MAX_DIAMETER;
 
-  thickness: number;
   junctionType: LineJunctionType;
-  junctionDiameter: number;
+  private _junctionDiameter: number;
 
   constructor(
     thickness: number = LineToolProperties.MIN_THICKNESS,
     junctionType: LineJunctionType = LineJunctionType.POINTS,
     junctionDiameter: number = LineToolProperties.MIN_DIAMETER,
   ) {
-    super('Line');
+    super('Line', LineToolProperties.MIN_THICKNESS, LineToolProperties.MAX_THICKNESS);
 
     this.thickness = thickness;
     this.junctionType = junctionType;
-    this.junctionDiameter = junctionDiameter;
+    this._junctionDiameter = junctionDiameter;
+  }
+
+  get junctionDiameter(): number {
+    return this._junctionDiameter;
+  }
+
+  set junctionDiameter(diameter: number) {
+    this._junctionDiameter = MathUtil.fit(diameter, this._minDiameter, this._maxDiameter);
+  }
+
+  get minDiameter(): number {
+    return this._minDiameter;
+  }
+
+  get maxDiameter(): number {
+    return this._maxDiameter;
   }
 }
---
+++ client/src/app/models/tool-properties/tool-properties.spec.ts
@@ -0,0 +1,48 @@
+import { ToolProperties } from './tool-properties';
+
+class MockToolProperties extends ToolProperties {
+  static readonly MIN_THICKNESS = 1;
+  static readonly MAX_THICKNESS = 50;
+
+  constructor(thickness: number = 1) {
+    super('MockTool', 1, 50);
+
+    this.thickness = thickness;
+  }
+}
+
+describe('Tool Properties', () => {
+  let toolProperties: MockToolProperties;
+
+  beforeEach(() => {
+    toolProperties = new MockToolProperties();
+  });
+
+  it('should have the correct tool name', () => {
+    expect(toolProperties.toolName).toBe('MockTool');
+  });
+
+  it('should correct a thickness input that is too low', () => {
+    toolProperties.thickness = toolProperties.minThickness - 1;
+    expect(toolProperties.thickness).toEqual(toolProperties.minThickness);
+  });
+
+  it('should correct a thickness input that is too high', () => {
+    toolProperties.thickness = toolProperties.maxThickness + 1;
+    expect(toolProperties.thickness).toEqual(toolProperties.maxThickness);
+  });
+
+  it('should keep a correct thickness input', () => {
+    const correctValue = (toolProperties.maxThickness - toolProperties.minThickness) / 2 + toolProperties.minThickness;
+    toolProperties.thickness = correctValue;
+    expect(toolProperties.thickness).toEqual(correctValue);
+  });
+
+  it('should return the correct minimum thickness', () => {
+    expect(toolProperties.minThickness).toBe(MockToolProperties.MIN_THICKNESS);
+  });
+
+  it('should return the correct maximum thickness', () => {
+    expect(toolProperties.maxThickness).toBe(MockToolProperties.MAX_THICKNESS);
+  });
+});
