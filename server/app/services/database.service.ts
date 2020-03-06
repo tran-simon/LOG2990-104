56b569a6907480999c8fc26eaad94fa698c38b29
Samuel Poulin <samuel.poulin@polymtl.ca>
2020-03-06 21:52:33 +0000

    LOG2990-101 code refactor

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -4,7 +4,7 @@ import * as express from 'express';
 import * as httpStatus from 'http-status-codes';
 
 import * as mongoose from 'mongoose';
-import drawingModel from '../../models/drawing';
+import drawingModel, { Drawing } from '../../models/drawing';
 
 @injectable()
 export class DatabaseService {
@@ -14,35 +14,21 @@ export class DatabaseService {
       'mongodb+srv://polydessin:letmein1@cluster0-lgpty.azure.mongodb.net/polydessin?retryWrites=true&w=majority',
       { useNewUrlParser: true, useUnifiedTopology: true },
       (err: mongoose.Error) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log('Connected to MongoDB');
-        }
+        err ? console.error(err.message) : console.log('Connected to MongoDB');
       });
   }
 
   getAllDrawings(res: express.Response): void {
-    drawingModel.find({}, (err, docs) => {
-      if (err) {
-        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-      } else if (docs) {
-        res.status(httpStatus.OK).json(docs);
-      } else {
-        res.sendStatus(httpStatus.NOT_FOUND);
-      }
+    drawingModel.find({}, (err: Error, docs: Drawing[]) => {
+      const status = this.determineStatus(err, docs);
+      docs ? res.status(status).json(docs) : res.sendStatus(status);
     });
   }
 
   getDrawingById(res: express.Response, id: string): void {
-    drawingModel.findById(id, (err, doc) => {
-      if (err) {
-        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-      } else if (doc) {
-        res.status(httpStatus.OK).json(doc);
-      } else {
-        res.sendStatus(httpStatus.NOT_FOUND);
-      }
+    drawingModel.findById(id, (err: Error, doc: Drawing) => {
+      const status = this.determineStatus(err, doc);
+      doc ? res.status(status).json(doc) : res.sendStatus(status);
     });
   }
 
@@ -56,20 +42,21 @@ export class DatabaseService {
   }
 
   deleteDrawing(res: express.Response, id: string): void {
-    drawingModel.findByIdAndDelete(id, (err, doc) => {
-      const status = err ? httpStatus.INTERNAL_SERVER_ERROR :
-        doc ? httpStatus.OK : httpStatus.NOT_FOUND;
-
-      res.sendStatus(status);
+    drawingModel.findByIdAndDelete(id, (err: Error, doc: Drawing) => {
+      res.sendStatus(this.determineStatus(err, doc));
     });
   }
 
   updateDrawing(res: express.Response, id: string, body: string): void {
-    drawingModel.findByIdAndUpdate(id, body, (err, doc) => {
-      const status = err ? httpStatus.INTERNAL_SERVER_ERROR :
-        doc ? httpStatus.OK : httpStatus.NOT_FOUND;
-
-      res.sendStatus(status);
+    drawingModel.findByIdAndUpdate(id, body, (err: Error, doc: Drawing) => {
+      res.sendStatus(this.determineStatus(err, doc));
     });
+  }
+
+  private determineStatus(err: Error, results: Drawing | Drawing[]): number {
+    const status = err ? httpStatus.INTERNAL_SERVER_ERROR :
+      results ? httpStatus.OK : httpStatus.NOT_FOUND;
+
+    return status;
   }
 }
