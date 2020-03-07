2d5a5b9389757d9d3d0e0ab4caf6ad69ce528b44
Simon Tran <simon.tran@polymtl.ca>
2020-03-07 03:47:26 -0500

    LOG2990-108 - Added tests for pipette and EditorComponent

--- client/src/app/components/pages/editor/drawing-surface/drawing-surface.component.ts
+++ client/src/app/components/pages/editor/drawing-surface/drawing-surface.component.ts
@@ -19,7 +19,7 @@ export class DrawingSurfaceComponent {
   svg: ElementRef;
 
   constructor() {
-    this.color = Color.WHITE;
+    this.color = Color.GREEN;
   }
 
   addShape(shape: BaseShape): void {
--- client/src/app/models/tools/other-tools/pipette-tool.ts
+++ client/src/app/models/tools/other-tools/pipette-tool.ts
@@ -7,43 +7,47 @@ import { Color } from 'src/app/utils/color/color';
  * Based on: https://stackoverflow.com/questions/3768565/drawing-an-svg-file-on-a-html5-canvas
  */
 export class PipetteTool extends Tool {
+  private image: HTMLImageElement;
 
   constructor(editorService: EditorService) {
     super(editorService);
+    this.image = new Image();
   }
 
   private pickColor(x: number, y: number, selectedColorType: SelectedColorType): void {
-    const {width, height} = this.editorService.view;
-    const img = new Image();
+    const {width, height, svg} = this.editorService.view;
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
     ctx.canvas.width = width;
     ctx.canvas.height = height;
 
-    const svg = this.editorService.view.svg.nativeElement;
-    const xml = new XMLSerializer().serializeToString(svg);
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0);
+    const xml = new XMLSerializer().serializeToString(svg.nativeElement);
+    this.image.onload = () => {
+      ctx.drawImage(this.image, 0, 0);
 
       const data = ctx.getImageData(x, y, 1, 1).data;
       const color = Color.rgb255(data[0], data[1], data[2]);
+
       this.editorService.colorsService.setColorByTypeAndUpdateHistory(color, selectedColorType);
     };
 
-    img.src = 'data:image/svg+xml;base64,' + btoa(xml);
-    img.style.display = 'none';
+    this.image.src = 'data:image/svg+xml;base64,' + btoa(xml);
+    this.image.style.display = 'none';
   }
 
   handleMouseEvent(e: MouseEvent): void {
     super.handleMouseEvent(e);
     if (e.type === 'click' || e.type === 'contextmenu') {
-      this.pickColor(e.offsetX, e.offsetY,
-        e.type === 'contextmenu' ?
-          SelectedColorType.secondary : SelectedColorType.primary);
+      if (this.editorService.view) {
+        this.pickColor(e.offsetX, e.offsetY,
+          e.type === 'contextmenu' ?
+            SelectedColorType.secondary : SelectedColorType.primary);
+      }
     }
   }
 
   protected updateProperties(): void {
     // todo remove
+    return;
   }
 }
