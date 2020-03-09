7445ddc7387b34af7c0b6a4ae913e0ac1865ba29
Samuel Poulin <samuel.poulin@polymtl.ca>
2020-03-09 10:47:46 -0400

    LOG2990-103 removed tests for now

--- client/src/app/components/pages/export-modal/export-modal.component.spec.ts
+++ client/src/app/components/pages/export-modal/export-modal.component.spec.ts
@@ -6,7 +6,6 @@ import createSpy = jasmine.createSpy;
 
 describe('ExportModalComponent', () => {
   const dialogRefCloseSpy = createSpy('close');
-  // let component: ExportModalComponent;
   let fixture: ComponentFixture<ExportModalComponent>;
 
   beforeEach(async(() => {
@@ -19,26 +18,7 @@ describe('ExportModalComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ExportModalComponent);
-    // component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
-  // it('Should call submit when Save button clicked', () => {
-  //   const submitClickSpy = spyOn(component, 'submit');
-  //   fixture.debugElement.nativeElement.querySelector('#saveButton').click();
-  //   expect(submitClickSpy).toHaveBeenCalledWith(false);
-  // });
-  // it('Should call submit when cancel button clicked', () => {
-  //   const submitClickSpy = spyOn(component, 'submit');
-  //   fixture.debugElement.nativeElement.querySelector('#cancelButton').click();
-  //   expect(submitClickSpy).toHaveBeenCalledWith(true);
-  // });
-  // it('Should call close dialog when saving drawing', () => {
-  //   component.submit(true);
-  //   expect(component.fullName).toEqual(component.name + '.' + component.selectedExtension);
-  //   expect(dialogRefCloseSpy).toHaveBeenCalledWith(component.fullName);
-  // });
 });
