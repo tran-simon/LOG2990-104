17df624b9ed2ba69227a1b0ece50c4925be24cc3
Theo St-Denis <theo.st-denis@polymtl.ca>
2020-03-08 21:30:34 -0400

    LOG2990-111 - Ready to push and merge

--- client/src/app/models/tool-properties/ellipse-tool-properties.ts
+++ client/src/app/models/tool-properties/ellipse-tool-properties.ts
@@ -2,7 +2,7 @@ import { ToolProperties } from 'src/app/models/tool-properties/tool-properties';
 import { ToolType } from '../tools/tool-type';
 
 export enum EllipseContourType {
-  FILLEDCONTOUR = 'Plein avec contour',
+  FILLED_CONTOUR = 'Plein avec contour',
   FILLED = 'Plein',
   CONTOUR = 'Contour',
 }
@@ -16,7 +16,10 @@ export class EllipseToolProperties extends ToolProperties {
 
   contourType: EllipseContourType;
 
-  constructor(thickness: number = EllipseToolProperties.MIN_THICKNESS, contourType: EllipseContourType = EllipseContourType.FILLEDCONTOUR) {
+  constructor(
+    thickness: number = EllipseToolProperties.MIN_THICKNESS,
+    contourType: EllipseContourType = EllipseContourType.FILLED_CONTOUR,
+  ) {
     super(ToolType.Ellipse);
 
     this.strokeWidth = thickness;
