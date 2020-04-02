9d39842c931fe4d7b432481fb6e1689e0acabce0
Simon Tran <simon.tran@polymtl.ca>
2020-04-01 21:37:21 -0400

    LOG2990-166 - Fixed numericpropertyinputcomponent allowing decimals with slider

--- client/src/app/components/shared/inputs/property-inputs/numeric-property-input/numeric-property-input.component.ts
+++ client/src/app/components/shared/inputs/property-inputs/numeric-property-input/numeric-property-input.component.ts
@@ -8,8 +8,14 @@ import { NumericProperty } from '@tool-properties/props/numeric-property/numeric
   styleUrls: ['./numeric-property-input.component.scss']
 })
 export class NumericPropertyInputComponent extends AbstractPropertyInput<NumericProperty> {
-  static readonly SLIDER_STEP: number = 0.1;
-
-  @Input() sliderStep: number = NumericPropertyInputComponent.SLIDER_STEP;
+  static readonly SLIDER_STEP: number = 1;
+  @Input() sliderStep: number;
   @Input() property: NumericProperty;
+  @Input() allowDecimals: boolean;
+
+  constructor() {
+    super();
+    this.allowDecimals = false;
+    this.sliderStep = NumericPropertyInputComponent.SLIDER_STEP
+  }
 }
