5e5166cbdb9c1d21dca4fffe27132fe70d3b425d
Philippe Savard <philippe-1.savard@polymtl.ca>
2020-04-03 17:21:32 -0400

    LOG2990-173 added grid-tool files and functions

--- client/src/app/models/tools/other-tools/grid-tool.ts
+++ client/src/app/models/tools/other-tools/grid-tool.ts
@@ -5,11 +5,15 @@ import { Tool } from '@tools/tool';
 export class GridTool extends Tool {
   toolProperties: GridToolProperties;
   upscaleGrid(): void {
+    // tslint:disable-next-line:no-magic-numbers
     this.editorService.view.smallGrid += 5;
+    // tslint:disable-next-line:no-magic-numbers
     this.editorService.view.largeGrid = this.editorService.view.smallGrid * 5;
   }
   downscaleGrid(): void {
+    // tslint:disable-next-line:no-magic-numbers
     this.editorService.view.smallGrid -= 5;
+    // tslint:disable-next-line:no-magic-numbers
     this.editorService.view.largeGrid = this.editorService.view.smallGrid * 5;
   }
   constructor(editorService: EditorService) {
