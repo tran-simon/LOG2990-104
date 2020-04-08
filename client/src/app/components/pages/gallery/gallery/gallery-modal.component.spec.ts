d346e87a5dcddba8a915c7dc8c5c8da925811803
Samuel Poulin <samuel.poulin@polymtl.ca>
2020-04-08 16:10:42 -0400

    LOG2990-201 changed tests for modalcomponent

--- client/src/app/components/pages/gallery/gallery/gallery-modal.component.spec.ts
+++ client/src/app/components/pages/gallery/gallery/gallery-modal.component.spec.ts
@@ -1,78 +1,64 @@
-// // tslint:disable: no-magic-numbers
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { MatDialogRef } from '@angular/material';
-// import { Router } from '@angular/router';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { SharedModule } from '@components/shared/shared.module';
-// import { Drawing } from '@models/drawing';
-// import { APIService } from '@services/api.service';
-// import { GalleryModule } from '../gallery.module';
-// import { GalleryModalComponent } from './gallery-modal.component';
+// tslint:disable: no-magic-numbers
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '@components/shared/shared.module';
+import { Drawing } from '@models/drawing';
+import { GalleryModule } from '../gallery.module';
+import { GalleryModalComponent } from './gallery-modal.component';
 
-// describe('Gallery Modal Component', () => {
-//   const dialogRefCloseSpy = jasmine.createSpy('close');
-//   const routerSpy = jasmine.createSpyObj('Router', {
-//     navigate: new Promise<boolean>(() => {
-//       return;
-//     }),
-//   });
-//   let component: GalleryModalComponent;
-//   let fixture: ComponentFixture<GalleryModalComponent>;
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [SharedModule, RouterTestingModule, GalleryModule],
-//       providers: [
-//         { provide: MatDialogRef, useValue: { close: dialogRefCloseSpy } },
-//         { provide: Router, useValue: routerSpy },
-//       ],
-//     }).compileComponents();
-//   }));
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(GalleryModalComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+describe('Gallery Modal Component', () => {
+  const dialogRefCloseSpy = jasmine.createSpy('close');
+  const routerSpy = jasmine.createSpyObj('Router', {
+    navigate: new Promise<boolean>(() => {
+      return;
+    }),
+  });
+  let component: GalleryModalComponent;
+  let fixture: ComponentFixture<GalleryModalComponent>;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [SharedModule, RouterTestingModule, GalleryModule],
+      providers: [
+        { provide: MatDialogRef, useValue: { close: dialogRefCloseSpy } },
+        { provide: Router, useValue: routerSpy },
+      ],
+    }).compileComponents();
 
-//   it('should route correctly when a drawing is chosen', () => {
-//     routerSpy.navigate.and.returnValue(Promise.resolve());
+    fixture = TestBed.createComponent(GalleryModalComponent);
 
-//     const drawing = new Drawing('testDrawing', [], '', 'ff0000', 100, 100, '', '123');
-//     component.chooseDrawing(drawing);
+    spyOn(GalleryModalComponent.prototype, 'fetchDrawings');
 
-//     fixture.whenStable().then(() => {
-//       fixture.detectChanges();
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//       expect(routerSpy.navigate).toHaveBeenCalledWith([
-//         'edit',
-//         {
-//           width: '100',
-//           height: '100',
-//           color: 'ff0000',
-//           id: '123',
-//         },
-//       ]);
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//       expect(dialogRefCloseSpy).toHaveBeenCalled();
-//     });
-//   });
+  it('should route correctly when a drawing is chosen', () => {
+    routerSpy.navigate.and.returnValue(Promise.resolve());
 
-//   it('should call getAllDrawings when fetchDrawings is called', () => {
-//     const spy = spyOn(APIService.prototype, 'getAllDrawings').and.returnValue(Promise.resolve([]));
+    const drawing = new Drawing('testDrawing', [], '', 'ff0000', 100, 100, '', '123');
+    component.chooseDrawing(drawing);
 
-//     component.fetchDrawings();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
 
-//     expect(spy).toHaveBeenCalled();
-//   });
+      expect(routerSpy.navigate).toHaveBeenCalledWith([
+        'edit',
+        {
+          width: '100',
+          height: '100',
+          color: 'ff0000',
+          id: '123',
+        },
+      ]);
 
-//   it('should call searchDrawings when updateDrawings is called', () => {
-//     const spy = spyOn(APIService.prototype, 'searchDrawings').and.returnValue(Promise.resolve([]));
-
-//     component.updateDrawings();
-
-//     expect(spy).toHaveBeenCalled();
-//   });
-// });
+      expect(dialogRefCloseSpy).toHaveBeenCalled();
+    });
+  });
+});
