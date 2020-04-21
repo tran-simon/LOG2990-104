e6c9bf57b8de89735872a4348bc1427bccc70f5b
Samuel Poulin <samuel-poulin@outlook.com>
2020-04-20 21:38:55 -0400

    LOG2990-218 added test for storage

--- client/src/app/services/localsave.service.spec.ts
+++ client/src/app/services/localsave.service.spec.ts
@@ -1,4 +1,6 @@
+// tslint:disable: no-magic-numbers
 import { async, TestBed } from '@angular/core/testing';
+import { Drawing } from '@models/drawing';
 import { LocalSaveService } from './localsave.service';
 
 describe('LocalSaveService', () => {
@@ -14,5 +16,20 @@ describe('LocalSaveService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should save the drawing on the browsers internal storage', () => {
+    const spy = spyOn(localStorage, 'setItem');
+    const drawing: Drawing = new Drawing('test', [], '', '', 100, 100, '');
+    service.takeSnapshot(drawing);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should load the drawing from the browsers internal storage', () => {
+    const drawing: Drawing = new Drawing('test', [], '', '', 100, 100, '');
+    service.takeSnapshot(drawing);
+
+    expect(service.drawing.name).toBe('test');
   });
 });
