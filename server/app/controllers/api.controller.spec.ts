b1f6e3df66cf83a5fb66e0fc284de96098ff9678
Theo St-Denis <theo.st-denis@polymtl.ca>
2020-04-20 21:26:13 -0400

    LOG2990-210 - Added tests to api controller

--- server/app/controllers/api.controller.spec.ts
+++ server/app/controllers/api.controller.spec.ts
@@ -2,17 +2,20 @@
 import * as chai from 'chai';
 import 'chai-http';
 chai.use(require('chai-http'));
-
-import { APIController } from './api.controller';
-
+import * as sinon from 'sinon';
 import { testingContainer } from '../../test/test-utils';
+import { Application } from '../app';
+import { EmailService } from '../services/email.service';
 import Types from '../types';
+import { APIController } from './api.controller';
 
 describe('API Controller', () => {
   let apiController: APIController;
-
+  // tslint:disable-next-line: prefer-const
+  let application: Application;
   beforeEach(async () => {
     await testingContainer().then((instance) => {
+      application = instance[0].get<Application>(Types.Application);
       apiController = instance[0].get<APIController>(Types.APIController);
     });
   });
@@ -21,4 +24,19 @@ describe('API Controller', () => {
     chai.expect(apiController).to.be.instanceOf(APIController);
     done();
   });
+  it('should call sendEmail when sending a POST request to api/email', (done: Mocha.Done) => {
+    const response = '';
+    const stub = sinon.stub(EmailService.prototype, 'sendEmail').resolves(response);
+    chai
+      .request(application.app)
+      .post('/api/email')
+      .set('content-type', 'application/json')
+      .send({ data: 'random data' })
+      .then(() => {
+        chai.expect(stub.called).to.equal(true);
+        stub.restore();
+        done();
+      });
+  });
+
 });
